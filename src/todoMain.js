947d54a72c8f7852ba228eb71f159ec459ff35d9
shabbirkhatri786 <yousufshabbir73@gmail.com>
2024-11-08 03:07:42 +0500

    complete functionality

--- src/todoMain.js
+++ src/todoMain.js
@@ -1,18 +1,55 @@
 import React, { useState } from 'react'
 
 function TodoMain() {
-  const [list, setList] = useState("");
-  const [todo, setTodo] = useState([]);
+  const [input, setInput] = useState("");
+  const [list, setList] = useState([]);
+  const [edit, setEdit] = useState(false);
+  const [index, setIndex] = useState(null);
 
-  const handleSave = (e) => {
-    setList(e.target.value);
+  const handleChange = (e) => {
+    setInput(e.target.value);
   };
 
-  const addButton = () => {
-    if (list !== '') {
-      setTodo([...todo, list]);
-      setList('');
-      console.log([...todo, list]);
+  const handleSave = () => {
+    if (input !== '')
+      setList([...list, input]);
+    setInput('');
+    console.log([...list, input]);
+
+  };
+
+  const handleDelete = (index) => {
+    const deleteItem = list.filter((e, i) => (index !== i))
+    setList(deleteItem);
+    console.log("delete item", index)
+  };
+
+
+
+  const handleEdit = (index) => {
+    const editItem = list.filter((e, i) => i == index)
+    setInput(editItem);
+    setIndex(index)
+    setEdit(true)
+
+
+  };
+
+  const handerUpdate = () => {
+    const editItem = list.map((e, i) => i == index ? input : e)
+    setList(editItem)
+    setIndex(null)
+    setInput("")
+    setEdit(false)
+  };
+
+  const handleEnterPress =(e)=>{
+    if(e.key === "Enter"){
+      if(edit){
+        handerUpdate();
+      }else{
+        handleSave();
+      }
     }
   };
 
@@ -20,17 +57,37 @@ function TodoMain() {
     <div>
       <h1>TodoApp</h1>
       <div className='main-container'>
-        <input className='input' type='text' onChange={handleSave} value={list} placeholder='Enter Text' />
-        <button onClick={addButton}>Add</button>
-        <div>
+        <input
+          className='input'
+          type='text'
+          onChange={handleChange}
+          value={input}
+          placeholder='Enter Text'
+          onKeyDown={handleEnterPress}
+          />
+        
 
-        </div>
-        <ul>
-          {todo.map((todo, index) => {
-            <li key={index}>{todo}</li>
-          })}
-        </ul>
+        {
+          edit ? <button onClick={handerUpdate}>Update</button> :
+            <button onClick={handleSave}>Add</button>
+        }
+
       </div>
+
+      {list.map((list, index) => {
+        return (
+          <div className="todo-item">
+            <ul className='list'>
+              
+              <li key={index}>{list}</li>
+              <button onClick={() => handleEdit(index)}>Edit</button>
+              <button onClick={() => handleDelete(index)}>Delete</button>
+
+            </ul>
+          </div>
+        )
+      })}
+
     </div>
   )
 }
